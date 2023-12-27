6733feea3d50ad5f6d945b01570d10af53e4c149
RoosterQMonee <82356323+RoosterQMonee@users.noreply.github.com>
2023-12-27 16:33:18 +0800

    Add files via upload

---
+++ Lethal Modder/LethalModder.Designer.cs
@@ -0,0 +1,258 @@
+﻿namespace Lethal_Modder
+{
+    partial class LethalModder
+    {
+        private System.ComponentModel.IContainer components = null;
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.MinimizeButton = new System.Windows.Forms.Button();
+            this.ExitButton = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.DeleteButton = new System.Windows.Forms.Button();
+            this.ModList = new System.Windows.Forms.ListBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.InstallButton = new System.Windows.Forms.Button();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.OutputLog = new System.Windows.Forms.ListBox();
+            this.StatusLabel = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.RemoveButton = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.panel3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(35)))), ((int)(((byte)(35)))), ((int)(((byte)(35)))));
+            this.panel1.Controls.Add(this.MinimizeButton);
+            this.panel1.Controls.Add(this.ExitButton);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Location = new System.Drawing.Point(-15, -15);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(608, 64);
+            this.panel1.TabIndex = 0;
+            this.panel1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseDown);
+            // 
+            // MinimizeButton
+            // 
+            this.MinimizeButton.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(45)))), ((int)(((byte)(45)))), ((int)(((byte)(45)))));
+            this.MinimizeButton.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.MinimizeButton.Font = new System.Drawing.Font("Bahnschrift SemiBold", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.MinimizeButton.ForeColor = System.Drawing.Color.LightGray;
+            this.MinimizeButton.Location = new System.Drawing.Point(522, 27);
+            this.MinimizeButton.Name = "MinimizeButton";
+            this.MinimizeButton.Size = new System.Drawing.Size(27, 25);
+            this.MinimizeButton.TabIndex = 3;
+            this.MinimizeButton.Text = "-";
+            this.MinimizeButton.UseVisualStyleBackColor = false;
+            this.MinimizeButton.Click += new System.EventHandler(this.MinimizeButton_Click);
+            // 
+            // ExitButton
+            // 
+            this.ExitButton.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(45)))), ((int)(((byte)(45)))), ((int)(((byte)(45)))));
+            this.ExitButton.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.ExitButton.Font = new System.Drawing.Font("Bahnschrift SemiBold", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ExitButton.ForeColor = System.Drawing.Color.IndianRed;
+            this.ExitButton.Location = new System.Drawing.Point(555, 27);
+            this.ExitButton.Name = "ExitButton";
+            this.ExitButton.Size = new System.Drawing.Size(27, 25);
+            this.ExitButton.TabIndex = 2;
+            this.ExitButton.Text = "X";
+            this.ExitButton.UseVisualStyleBackColor = false;
+            this.ExitButton.Click += new System.EventHandler(this.ExitButton_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Cascadia Mono Light", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.SystemColors.ButtonShadow;
+            this.label2.Location = new System.Drawing.Point(188, 30);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(104, 17);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "made by Nami";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Cascadia Mono SemiBold", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label1.Location = new System.Drawing.Point(27, 24);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(155, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Lethal Modder";
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(45)))), ((int)(((byte)(45)))), ((int)(((byte)(45)))));
+            this.panel2.Controls.Add(this.RemoveButton);
+            this.panel2.Controls.Add(this.DeleteButton);
+            this.panel2.Controls.Add(this.ModList);
+            this.panel2.Controls.Add(this.label3);
+            this.panel2.Controls.Add(this.InstallButton);
+            this.panel2.Location = new System.Drawing.Point(17, 67);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(374, 228);
+            this.panel2.TabIndex = 1;
+            // 
+            // DeleteButton
+            // 
+            this.DeleteButton.Location = new System.Drawing.Point(291, 10);
+            this.DeleteButton.Name = "DeleteButton";
+            this.DeleteButton.Size = new System.Drawing.Size(65, 21);
+            this.DeleteButton.TabIndex = 0;
+            this.DeleteButton.Text = "Delete";
+            this.DeleteButton.UseVisualStyleBackColor = true;
+            // 
+            // ModList
+            // 
+            this.ModList.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(50)))), ((int)(((byte)(50)))), ((int)(((byte)(50)))));
+            this.ModList.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.ModList.Font = new System.Drawing.Font("Cascadia Mono", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ModList.ForeColor = System.Drawing.SystemColors.Window;
+            this.ModList.FormattingEnabled = true;
+            this.ModList.ItemHeight = 17;
+            this.ModList.Location = new System.Drawing.Point(15, 40);
+            this.ModList.Name = "ModList";
+            this.ModList.Size = new System.Drawing.Size(341, 170);
+            this.ModList.TabIndex = 5;
+            this.ModList.SelectedIndexChanged += new System.EventHandler(this.ModList_SelectedIndexChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Cascadia Mono", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label3.Location = new System.Drawing.Point(12, 10);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(73, 21);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "ModList";
+            // 
+            // InstallButton
+            // 
+            this.InstallButton.Location = new System.Drawing.Point(91, 10);
+            this.InstallButton.Name = "InstallButton";
+            this.InstallButton.Size = new System.Drawing.Size(86, 21);
+            this.InstallButton.TabIndex = 6;
+            this.InstallButton.Text = "Setup Mods";
+            this.InstallButton.UseVisualStyleBackColor = true;
+            this.InstallButton.Click += new System.EventHandler(this.InstallButton_Click);
+            // 
+            // panel3
+            // 
+            this.panel3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(45)))), ((int)(((byte)(45)))), ((int)(((byte)(45)))));
+            this.panel3.Controls.Add(this.OutputLog);
+            this.panel3.Controls.Add(this.StatusLabel);
+            this.panel3.Controls.Add(this.label4);
+            this.panel3.Location = new System.Drawing.Point(407, 67);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(157, 228);
+            this.panel3.TabIndex = 0;
+            // 
+            // OutputLog
+            // 
+            this.OutputLog.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(50)))), ((int)(((byte)(50)))), ((int)(((byte)(50)))));
+            this.OutputLog.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.OutputLog.Font = new System.Drawing.Font("Cascadia Mono", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.OutputLog.ForeColor = System.Drawing.SystemColors.Window;
+            this.OutputLog.FormattingEnabled = true;
+            this.OutputLog.ItemHeight = 17;
+            this.OutputLog.Location = new System.Drawing.Point(17, 40);
+            this.OutputLog.Name = "OutputLog";
+            this.OutputLog.Size = new System.Drawing.Size(125, 170);
+            this.OutputLog.TabIndex = 6;
+            // 
+            // StatusLabel
+            // 
+            this.StatusLabel.AutoSize = true;
+            this.StatusLabel.Font = new System.Drawing.Font("Cascadia Mono Light", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.StatusLabel.ForeColor = System.Drawing.Color.LightGreen;
+            this.StatusLabel.Location = new System.Drawing.Point(78, 10);
+            this.StatusLabel.Name = "StatusLabel";
+            this.StatusLabel.Size = new System.Drawing.Size(72, 17);
+            this.StatusLabel.TabIndex = 4;
+            this.StatusLabel.Text = "Unloaded";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Cascadia Mono", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label4.Location = new System.Drawing.Point(8, 7);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(73, 21);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Status:";
+            // 
+            // RemoveButton
+            // 
+            this.RemoveButton.Location = new System.Drawing.Point(91, 10);
+            this.RemoveButton.Name = "RemoveButton";
+            this.RemoveButton.Size = new System.Drawing.Size(86, 21);
+            this.RemoveButton.TabIndex = 7;
+            this.RemoveButton.Text = "Remove Mods";
+            this.RemoveButton.UseVisualStyleBackColor = true;
+            this.RemoveButton.Click += new System.EventHandler(this.RemoveButton_Click);
+            // 
+            // LethalModder
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(75)))), ((int)(((byte)(75)))), ((int)(((byte)(75)))));
+            this.ClientSize = new System.Drawing.Size(579, 312);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "LethalModder";
+            this.Text = "Lethal Modder";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.LethalModder_FormClosed);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.panel3.ResumeLayout(false);
+            this.panel3.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button MinimizeButton;
+        private System.Windows.Forms.Button ExitButton;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.ListBox ModList;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Button DeleteButton;
+        private System.Windows.Forms.Label StatusLabel;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button InstallButton;
+        private System.Windows.Forms.ListBox OutputLog;
+        private System.Windows.Forms.Button RemoveButton;
+    }
+}
+
---
+++ Lethal Modder/LethalModder.cs
@@ -0,0 +1,276 @@
+﻿using System;
+using System.Drawing;
+using System.IO;
+using System.IO.Compression;
+using System.Linq;
+using System.Net;
+using System.Runtime.InteropServices;
+using System.Windows.Forms;
+
+
+namespace Lethal_Modder
+{
+    public partial class LethalModder : Form
+    {
+        #region Globals
+
+        FileSystemWatcher watcher;
+
+        private bool IsInstalled = false;
+        private bool IsAwaiting = false;
+
+        private string BepInExURL = "https://github.com/RoosterQMonee/Lethal-Modder/raw/main/Dependencies/BepInEx-5.4.2100.zip";
+        private string GameLocation = "";
+        private string RelativeGameLocation = "/steamapps/common/Lethal Company";
+        private string SteamLocation = "";
+        private string[] SteamLocations = { @"C:/Program Files/Steam", @"C:/Program Files (x86)/Steam", @"D:/Program Files/Steam", @"D:/Program Files (x86)/Steam" };
+        
+        public const int WM_NCLBUTTONDOWN = 0xA1;
+        public const int HT_CAPTION = 0x2;
+
+        #endregion
+        #region Imports
+
+        [DllImport("Gdi32.dll", EntryPoint = "CreateRoundRectRgn")]
+        private static extern IntPtr CreateRoundRectRgn
+        (
+            int nLeftRect,
+            int nTopRect,
+            int nRightRect,
+            int nBottomRect,
+            int nWidthEllipse,
+            int nHeightEllipse
+        );
+
+        [System.Runtime.InteropServices.DllImport("user32.dll")]
+        public static extern int SendMessage(IntPtr hWnd, int Msg, int wParam, int lParam);
+
+        [System.Runtime.InteropServices.DllImport("user32.dll")]
+        public static extern bool ReleaseCapture();
+
+
+        #endregion
+
+        public LethalModder()
+        {
+            InitializeComponent();
+            Region = System.Drawing.Region.FromHrgn(CreateRoundRectRgn(0, 0, Width, Height, 20, 20));
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            OutputLog.Items.Add("Loaded Window!");
+
+            foreach (string file_location in SteamLocations)
+            {
+                if (Directory.Exists(file_location))
+                {
+                    SteamLocation = file_location;
+                    OutputLog.Items.Add("Found Steam!");
+
+                    if (Directory.Exists(file_location + RelativeGameLocation))
+                    {
+                        GameLocation = file_location + RelativeGameLocation;
+                        OutputLog.Items.Add("Found LC!");
+                    }
+
+                    else
+                        OutputLog.Items.Add("Unable to find LC.");
+
+                    break;
+                }
+            }
+
+            if (SteamLocation == "")
+                OutputLog.Items.Add("Unable to find Steam.");
+
+            IsInstalled = Directory.Exists(GameLocation + "/BepInEx");
+            UpdateStatus();
+
+            if (IsInstalled)
+                UpdateModList();
+
+            IsAwaiting = IsInstalled;
+
+            if (IsAwaiting)
+                WatchModFolder();
+        }
+
+        private void InstallButton_Click(object sender, EventArgs e)
+        {
+            CheckInstallation();
+
+            using (WebClient client = new WebClient())
+                client.DownloadFile(BepInExURL, GameLocation + "/BepInEx.zip");
+
+            if (File.Exists(GameLocation + "/BepInEx.zip"))
+                OutputLog.Items.Add("Loaded BepInEx!");
+
+            else
+                OutputLog.Items.Add("Failed download.");
+
+            ZipFile.ExtractToDirectory(GameLocation + "/BepInEx.zip", GameLocation);
+            File.Delete(GameLocation + "/BepInEx.zip");
+
+            IsInstalled = Directory.Exists(GameLocation + "/BepInEx");
+            UpdateStatus();
+
+            IsAwaiting = IsInstalled;
+
+            if (IsAwaiting)
+                WatchModFolder();
+        }
+        
+        private void RemoveButton_Click(object sender, EventArgs e)
+        {
+            CheckInstallation();
+
+            if (!Directory.Exists(GameLocation + "/BepInEx"))
+                return;
+            
+            try
+            {
+                watcher.Changed -= OnNewModAdded;
+                watcher.Dispose();
+            }
+            catch { } // same error, every time.
+
+
+            Directory.Delete(GameLocation + "/BepInEx", true);
+
+            File.Delete(GameLocation + "/doorstop_config.ini");
+            File.Delete(GameLocation + "/winhttp.dll");
+
+            OutputLog.Items.Add("Deleted Mods!");
+
+            IsInstalled = Directory.Exists(GameLocation + "/BepInEx");
+            IsAwaiting = false;
+
+            UpdateStatus();
+            UpdateModList();
+        }
+
+        #region Prototypes
+
+        private void ModList_SelectedIndexChanged(object sender, EventArgs e) { }
+
+        #endregion
+        #region Window Handlers
+
+        private void ExitButton_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void MinimizeButton_Click(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Minimized;
+        }
+        private void panel1_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                ReleaseCapture();
+                SendMessage(Handle, WM_NCLBUTTONDOWN, HT_CAPTION, 0);
+            }
+        }
+
+        #endregion
+        #region Utils
+
+        private void CheckInstallation()
+        {
+            if (GameLocation == "")
+            {
+                MessageBox.Show("Cannot find LC installation! Please select the game's exe.");
+
+                using (OpenFileDialog diag = new OpenFileDialog())
+                {
+                    diag.InitialDirectory = @"c:\\";
+                    diag.Filter = "exe files (*.exe)|*.exe";
+                    diag.FilterIndex = 2;
+                    diag.RestoreDirectory = true;
+
+                    if (diag.ShowDialog() == DialogResult.OK)
+                        GameLocation = diag.FileName.Replace("\\Lethal Company.exe", "");
+                }
+            }
+        }
+
+        private void UpdateStatus()
+        {
+            StatusLabel.Text = IsInstalled ? "Enabled" : "Disabled";
+            StatusLabel.ForeColor = IsInstalled ? Color.LightGreen : Color.IndianRed;
+
+            InstallButton.Enabled = !IsInstalled;
+            InstallButton.Visible = !IsInstalled;
+
+            RemoveButton.Enabled = IsInstalled;
+            RemoveButton.Visible = IsInstalled;
+
+            DeleteButton.Enabled = IsInstalled;
+            DeleteButton.Visible = IsInstalled;
+        }
+
+        private void UpdateModList()
+        {
+            ModList.Items.Clear();
+
+            if (!Directory.Exists(GameLocation + "/BepInEx") || !Directory.Exists(GameLocation + "/BepInEx/plugins"))
+                return;
+
+            foreach (string mod in Directory.EnumerateDirectories(GameLocation + "/BepInEx/plugins"))
+                ModList.Items.Add(mod.Replace(".dll", "").Split('/').Last<string>());
+
+            foreach (string mod in Directory.EnumerateFiles(GameLocation + "/BepInEx/plugins"))
+                ModList.Items.Add(mod.Replace(".dll", "").Split('/').Last<string>());
+        }
+
+
+        #endregion
+        #region Background
+
+        private void OnNewModAdded(object source, FileSystemEventArgs e)
+        {
+            try
+            {
+                this.Invoke(new MethodInvoker(delegate
+                {
+                    UpdateModList();
+                }));
+            }
+            catch { } // nothing happens.
+        }
+
+        private void WatchModFolder()
+        {
+
+            if (Directory.Exists(GameLocation + "/BepInEx") && !Directory.Exists(GameLocation + "/BepInEx/plugins"))
+                Directory.CreateDirectory(GameLocation + "/BepInEx/plugins");
+
+            if (!Directory.Exists(GameLocation + "/BepInEx/plugins"))
+                return;
+
+            watcher = new FileSystemWatcher();
+
+            watcher.Path = GameLocation + "/BepInEx/plugins";
+            watcher.Filter = "*.dll";
+            
+            watcher.Changed += new FileSystemEventHandler(OnNewModAdded);
+            watcher.Deleted += new FileSystemEventHandler(OnNewModAdded);
+
+            watcher.EnableRaisingEvents = true;
+        }
+
+        private void LethalModder_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            if (!IsAwaiting)
+                return;
+
+            watcher.Changed -= OnNewModAdded;
+            watcher.Dispose();
+        }
+
+        #endregion
+    }
+}
